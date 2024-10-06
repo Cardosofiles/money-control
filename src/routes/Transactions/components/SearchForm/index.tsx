eff0ccc42c231142c6e9dc12e8495f08a0ce97ef
João Batista <cardosofiles@outlook.com>
2024-10-06 09:55:10 -0300

    adding use-context-selector, useMemo for performance

--- src/routes/Transactions/components/SearchForm/index.tsx
+++ src/routes/Transactions/components/SearchForm/index.tsx
@@ -1,5 +1,5 @@
-import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { useContextSelector } from "use-context-selector";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
@@ -16,7 +16,12 @@ const searchFormSchema = z.object({
 type SearchFormInput = z.infer<typeof searchFormSchema>;
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionsContext);
+  const fetchTransactions = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.fetchTransactions;
+    }
+  );
 
   const {
     register,
--- src/routes/Transactions/index.tsx
+++ src/routes/Transactions/index.tsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-
+import { useContextSelector } from "use-context-selector";
 import { Header } from "../../components/Header";
 import { Sumarry } from "../../components/Summary";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
@@ -12,7 +11,9 @@ import {
 } from "./styles";
 
 export function Transactions() {
-  const { transactions } = useContext(TransactionsContext);
+  const transactions = useContextSelector(TransactionsContext, (context) => {
+    return context.transactions;
+  });
 
   return (
     <div>
